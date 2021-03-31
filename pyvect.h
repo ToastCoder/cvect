2d9ac9267e4b98e123254e747a701e44616569a6
ToastCoder <vicky.pcbasic@gmail.com>
2021-03-31 21:54:23 +0530

    More optimizations

--- pyvect.h
+++ pyvect.h
@@ -41,17 +41,12 @@ struct reci
     double *rec3;
 };
 
-typedef struct unormal u_norm;
-typedef struct reci recip;
-
 // FUNCTION TO CALCULATE THE DOT PRODUCT
 int dot(int *x, int *y)
 {
     int dot_product;
     for(int i = 0; i<3; i++)
-    {
         dot_product = dot_product + (x[i]*y[i]);
-    }
     return dot_product;
 }
 
@@ -86,28 +81,14 @@ double projection(int *x, int *y)
 // FUNCTION TO FIND IF TWO VECTORS ARE PREPENDICULAR
 bool isPerpendicular(int *x, int *y)
 {
-    if (dot(x,y) == 0)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
+    (dot(x,y) == 0)?return true:return false;
 }
 
 // FUNCTION TO FIND IF TWO VECTORS ARE COLLINEAR
 bool isCollinear(int *x,int *y)
 {
     int *res = cross(x,y);
-    if ((res[0]==0)||(res[1]==0)||(res[2]==0))
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+    ((res[0]==0)||(res[1]==0)||(res[2]==0))?return true:return false;
 }
 
 // FUNCTION TO FIND UNIT VECTOR
@@ -115,21 +96,17 @@ double *unitVector(int *arr)
 {
     double *unit;
     for(int i=0;i<3;i++)
-    {
         unit[i] = unit[i]/modVector(arr);
-    }
     return unit;
 }
 
 // FUNCTION TO FIND UNIT NORMAL
-u_norm unitNormal(int *x, int *y)
+struct unormal unitNormal(int *x, int *y)
 {
-    u_norm un;
+    unormal un;
     un.u_norm = unitVector(cross(x,y));
     for (int i=0;i<3;i++)
-    {
         un.u_norm_inv[i] = un.u_norm[i]*-1;
-    }
     return un;
 }
 
@@ -140,9 +117,7 @@ double *bisector(int *x, int *y)
     double *unit_a = unitVector(x);
     double *unit_b = unitVector(y);
     for (int i=0;i<3;i++)
-    {
         bsect[i] = unit_a[i]+unit_b[i];
-    }
     return bsect;
 }
 
@@ -151,33 +126,23 @@ double *positionVector(int *x, int *y)
 {
     double *pos;
     for(int i=0;i<3;i++)
-    {
         pos[i] = 0.5*(x[i]+y[i]);
-    }
     return pos;
 }
 
 // FUNCTION TO FIND IF TWO VECTORS ARE COPLANAR
 bool isCoplanar(int *x, int *y, int *z)
 {
-    if (dot(cross(x,y),z) == 0)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
+    (dot(cross(x,y),z) == 0)?return true:return false;
 }
 
 // FUNCTION TO FIND THE RECIPROCAL OF THREE VECTORS
-recip reciprocal(int *x, int *y, int *z)
+struct reci reciprocal(int *x, int *y, int *z)
 {
-    recip r;
+    reci r;
     int *c1 = cross(y,z);
     int *c2 = cross(z,x);
     int *c3 = cross(x,y);
-
     for (int i=0;i<3;i++)
     {
         r.rec1[i] = c1[i]/dot(cross(x,y),z);
