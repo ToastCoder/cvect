00d01eb232097729a7f8594120e336c9534fa151
ToastCoder <vicky.pcbasic@gmail.com>
2021-03-31 12:42:06 +0530

    Some changes

---
+++ cent.h
@@ -0,0 +1,28 @@
+/////////////////////////////////////////////////////
+
+// PYVECT-C
+
+// FILE NAME: CENT.H
+
+// DEVELOPED BY: VIGNESHWAR RAVICHANDAR
+
+// LICENSE: MIT LICENSE
+
+/////////////////////////////////////////////////////
+
+// SUB HEADER FILE FOR CENT FUNCTIONS
+
+// HEADER FILE DEFINITION
+#ifndef CENT_H
+#define CENT_H
+
+// REQUIRED LIBRARY FILES
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdbool.h>
+
+// REQUIRED SUBHEADERS
+#include "pyvect.h"
+
+#endif
