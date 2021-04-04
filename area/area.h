37934727973832e38bf04add03be7e777358220b
ToastCoder <vicky.pcbasic@gmail.com>
2021-04-04 18:32:10 +0530

    Some changes

--- area/area.h
+++ area/area.h
@@ -1,6 +1,6 @@
 /////////////////////////////////////////////////////
 
-// PYVECT-C
+// CVECT
 
 // FILE NAME: AREA.H
 
@@ -23,7 +23,7 @@
 #include <stdbool.h>
 
 // REQUIRED SUBHEADERS
-#include "../pyvect.h"
+#include "../cvect.h"
 
 // FUNCTION TO FIND AREA OF TRIANGLE WHERE ADJACENT SIDES ARE GIVEN
 double triangle_adj(int *arr1, int *arr2) 
