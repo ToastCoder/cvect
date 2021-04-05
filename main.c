aeba8e77eaa63dd854a87224ea0111da6a555a1b
Moulishankar10 <moulishankarmr@gmail.com>
2021-04-05 19:06:09 +0530

    Fixed a bug

--- main.c
+++ main.c
@@ -6,15 +6,18 @@
 #include "cvect.h"
 void tester(int a1[3],int a2[3], int a3[3], int a4[3])
 {
-    int *arr = cross(a1,a2);
-    for(int i=0;i<3;i++) printf("%d",arr[i]);
+    double a = modVector(a1);
+    //for(int i=0;i<3;i++) printf("%lf",arr[i]);
+    printf("%lf",a);
 }
 int main()
 {
-    int a1[3] = {1,2,3};
-    int a2[3] = {4,5,6};
-    int a3[3] = {7,8,9};
-    int a4[3] = {1,5,9};
+    int a1[] = {1,2,3};
+    int a2[] = {4,5,6};
+    int a3[] = {7,8,9};
+    int a4[] = {1,5,9};
+    double res = sqrt(4.54);
+    //printf("%lf",res);
     tester(a1,a2,a3,a4);
     return 0;
 }
