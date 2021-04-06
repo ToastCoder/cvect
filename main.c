14b86cbf40f9c62f7f25b561307d0fd815dfb9cb
Moulishankar10 <moulishankarmr@gmail.com>
2021-04-06 14:28:16 +0530

    Modified the code

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ void tester(int a1[],int a2[], int a3[], int a4[])
     printf("Dot Product : %d\n",dot(a1,a2));
 
     // CROSS TESTER
-    int *c = cross(a1,a2);
+    int *c = cross(a2,a1);
     printf("Cross Product : ");
     for(int i=0;i<3;i++){printf("%d ",c[i]);}
     printf("\n");
@@ -25,7 +25,7 @@ void tester(int a1[],int a2[], int a3[], int a4[])
     printf("Projection : %f\n",projection(a2,a3));
 
     //ISPERPENDICULAR TESTER
-    printf("IsPerpendicular : %s\n",isPerpendicular(a3,a4));
+    printf("IsPerpendicular : %s\n",isPerpendicular(a2,a4));
 
 }
 int main()
@@ -33,9 +33,11 @@ int main()
     int a1[] = {1,2,3};
     int a2[] = {4,5,6};
     int a3[] = {7,8,9};
-    int a4[] = {1,5,1};
+    int a4[] = {0,0,0};
     
-    //printf("%lf",acos(1));
+    //printf("%d\n",cross(a2,a4)[2]);
+
     tester(a1,a2,a3,a4);
+
     return 0;
 }
