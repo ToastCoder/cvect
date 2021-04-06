5494a245494a067561c590360109c5c1e383812a
Moulishankar10 <moulishankarmr@gmail.com>
2021-04-06 14:08:58 +0530

    Modified the code

--- main.c
+++ main.c
@@ -24,6 +24,9 @@ void tester(int a1[],int a2[], int a3[], int a4[])
     // PRPJECTION TESTER
     printf("Projection : %f\n",projection(a2,a3));
 
+    //ISPERPENDICULAR TESTER
+    printf("IsPerpendicular : %s\n",isPerpendicular(a3,a4));
+
 }
 int main()
 {
