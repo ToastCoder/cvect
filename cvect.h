41d146b5a2b84423f1eeb5ae00c4348f5fa278b0
Moulishankar10 <moulishankarmr@gmail.com>
2021-04-06 13:40:17 +0530

    Fixed a bug

--- cvect.h
+++ cvect.h
@@ -74,7 +74,7 @@ float modVector(int *x)
 // FUNCTION TO FIND ANGLE BETWEEN TWO VECTORS
 float angle(int *x,int *y)
 { 
-    return (dot(x,y)/(modVector(x)*modVector(y)));
+    return (float)(dot(x,y)/(modVector(x)*modVector(y)));
 }
 
 // FUNCTION TO FIND THE PROJECTION OF ONE VECTOR OVER ANOTHER
