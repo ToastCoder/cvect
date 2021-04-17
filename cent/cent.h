0dc7590dac64970268fd8453e18080d108cacfc9
ToastCoder <vicky.pcbasic@gmail.com>
2021-04-17 18:05:20 +0530

    Some changes

--- cent/cent.h
+++ cent/cent.h
@@ -33,9 +33,9 @@ float *triangle(int *arr1, int *arr2, int *arr3)
     return res;
 }
 
-double *tetrahedron(int *arr1, int *arr2, int *arr3, int *arr4)
+float *tetrahedron(int *arr1, int *arr2, int *arr3, int *arr4)
 {
-    double *res;
+    static float res[3];
     for(int i=0;i<3;i++)
         res[i] = 0.25*(arr1[i]+arr2[i]+arr3[i]+arr4[i]);
     return res;
