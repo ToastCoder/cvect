6f70e35d455e222eb2eb3abfe0e23199665f9e55
ToastCoder <vicky@Vicky-Desktop.localdomain>
2021-04-17 15:56:29 +0530

    Some changes

--- main.c
+++ main.c
@@ -58,6 +58,16 @@ void tester(int a1[],int a2[], int a3[], int a4[])
         printf("%f ",bs[i]);
     }
     printf("\n");
+
+    //POSITION TESTER
+    float *pv;
+    pv = positionVector(a1,a2);
+    printf("Position Vector: \n");
+    for (int i=0;i<3;i++)
+    {
+        printf("%f ",pv[i]);
+    }
+    printf("\n");
 }
 
    
