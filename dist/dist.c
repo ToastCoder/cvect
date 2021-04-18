919715952f0a9abc56b798f4948dcac08203e2cb
Moulishankar10 <moulishankarmr@gmail.com>
2021-04-18 19:07:58 +0530

    Modified the code

--- dist/dist.c
+++ dist/dist.c
@@ -7,3 +7,12 @@
 // REQUIRED SUBHEADERS
 #include "dist.h"
 #include "../cvect.h"
+
+// FUNCTION TO THE DISTANCE BETWEEN TWO PARALLEL LINES
+float pl_line(int *a1, int *a2, int *u)
+{
+    int d[3];
+    for(int i=0;i<3;i++) d[i] = a2[i] - a1[i];
+    float x = modVector(cross(u,d));
+    return x/modVector(u);
+}
