71cb72ecc614ed4851a5407c9cae000e26bed88a
Moulishankar10 <moulishankarmr@gmail.com>
2021-04-18 18:50:38 +0530

    Created dist.h

---
+++ dist/dist.h
@@ -0,0 +1,28 @@
+/////////////////////////////////////////////////////
+
+// CVECT
+
+// FILE NAME: DIST.H
+
+// DEVELOPED BY: VIGNESHWAR RAVICHANDAR
+
+// LICENSE: MIT LICENSE
+
+/////////////////////////////////////////////////////
+
+// SUB HEADER FILE FOR AREA FUNCTIONS
+
+// HEADER FILE DEFINITION
+#ifndef DIST_H
+#define DIST_H
+
+// REQUIRED LIBRARY FILES
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdbool.h>
+
+// REQUIRED SUBHEADERS
+#include "../cvect.h"
+
+#endif
