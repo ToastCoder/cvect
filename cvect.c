a33cdf0b4937ed561c3ddf87e95e7fe043747a1d
Moulishankar10 <moulishankarmr@gmail.com>
2021-04-18 15:00:53 +0530

    Updated the code

--- cvect.c
+++ cvect.c
@@ -32,7 +32,7 @@ int *cross(int *x, int *y)
 {
     static int cross_product[3];
     cross_product[0] = x[1]*y[2] - x[2]*y[1];
-    cross_product[1] = (x[2]*x[0] - x[0]*y[2])*-1;
+    cross_product[1] = (x[0]*y[2] - x[2]*y[0])*-1;
     cross_product[2] = x[0]*y[1] - x[1]*y[0];
     return cross_product;
 }
