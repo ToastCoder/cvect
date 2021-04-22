b9830f83d24a105c85674d205113dce140de2ee0
Moulishankar10 <moulishankarmr@gmail.com>
2021-04-22 11:55:27 +0530

    Updated the code

--- prod/prod.c
+++ prod/prod.c
@@ -26,7 +26,7 @@ int s3(int *x, int *y, int *z)
     return dot(cross(x,y),z);
 }
 
-// FUNCTION TO FIND SCALAR PRODUCT FOR THE GIVEN FOUR VECTORS
+// FUNCTION TO FIND SCALAR QUADRUPLE PRODUCT FOR THE GIVEN FOUR VECTORS
 int s4(int *w, int *x, int *y, int *z)
 {
     int c1[3], c2[3];
@@ -41,4 +41,13 @@ int *v3(int *x, int *y, int *z)
     int c1[3];
     for(int i=0;i<3;i++) c1[i] = cross(x,y)[i];
     return cross(c1,z);
+}
+
+// FUNCTION TO FIND VECTOR QUADRUPLE PRODUCT FOR THE GIVEN FOUR VECTORS
+int *v4(int *w, int *x, int *y, int *z)
+{
+    int c1[3], c2[3];
+    for(int i=0;i<3;i++) c1[i] = cross(w,x)[i];
+    for(int i=0;i<3;i++) c2[i] = cross(y,z)[i];
+    return (cross(c1,c2));
 }
