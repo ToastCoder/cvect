73e4ca24be625737e0510bacd71e265b5aa6dc71
ToastCoder <vicky.pcbasic@gmail.com>
2021-04-22 09:24:15 +0530

    Some changes

---
+++ prod/prod.c
@@ -0,0 +1,21 @@
+/////////////////////////////////////////////////////
+
+// CVECT
+
+// FILE NAME: PROD.C
+
+// DEVELOPED BY: VIGNESHWAR RAVICHANDAR, MOULISHANKAR M R
+
+// LICENSE: MIT LICENSE
+
+/////////////////////////////////////////////////////
+
+// REQUIRED LIBRARY FILES
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdbool.h>
+
+// REQUIRED SUBHEADERS
+#include "prod.h"
+#include "../cvect.h"
---
+++ prod/prod.h
@@ -0,0 +1,20 @@
+/////////////////////////////////////////////////////
+
+// CVECT
+
+// FILE NAME: PROD.H
+
+// DEVELOPED BY: VIGNESHWAR RAVICHANDAR, MOULISHANKAR M R
+
+// LICENSE: MIT LICENSE
+
+/////////////////////////////////////////////////////
+
+// REQUIRED LIBRARY FILES
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdbool.h>
+
+// REQUIRED SUBHEADERS
+#include "../cvect.h"
