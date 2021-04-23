1c23e210adc0061e144d2269647d30808d41a3e7
ToastCoder <vicky.pcbasic@gmail.com>
2021-04-23 14:50:26 +0530

    Some changes

---
+++ volume/volume.h
@@ -0,0 +1,29 @@
+/////////////////////////////////////////////////////
+
+// CVECT
+
+// FILE NAME: VOLUME.H
+
+// DEVELOPED BY: VIGNESHWAR RAVICHANDAR, MOULISHANKAR M R
+
+// LICENSE: MIT LICENSE
+
+/////////////////////////////////////////////////////
+
+// SUB HEADER FILE FOR VOLUME FUNCTIONS
+
+// HEADER FILE DEFINITION
+#ifndef VOLUME_H
+#define VOLUME_H
+
+// REQUIRED LIBRARY FILES
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdbool.h>
+
+// REQUIRED SUBHEADERS
+#include "../cvect.h"
+#include "../section/section.h"
+
+#endif
