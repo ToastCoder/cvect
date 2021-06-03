cd5870cddcc567a78a16f8aea82956f3ceba35fb
ToastCoder <vicky.pcbasic@gmail.com>
2021-06-03 10:27:49 +0530

    Added test for isCoplanar()

--- main.cpp
+++ main.cpp
@@ -73,6 +73,9 @@ void tester(int a1[],int a2[], int a3[], int a4[])
         printf("%d ",pv[i]);
     }
     printf("\n");
+
+    // ISCOPLANAR TESTER
+    printf("IsCoplanar : %d\n",isCoplanar(a1,a2,a3));
 }
 
 int main()
